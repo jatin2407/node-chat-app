11b49342a770fc37f655a532dbc14cbba2b5369b
jatin <jatinpatel816@gmail.com>
2017-12-25 12:44:43 +0530

    added time to the app

---
+++ playground/time.js
@@ -0,0 +1,5 @@
+const moment = require('moment');
+//Jan 1st 1970 00:00:00 am
+var date = moment();
+
+console.log(date.format('MMM Do YYYY HH:mm:ss a'));
--- server/utils/message.js
+++ server/utils/message.js
@@ -1,15 +1,18 @@
+var moment = require('moment');
+var date = moment();
+
 var generateMessage = (from , text) => {
   return {
     from,
     text,
-    createdAt : new Date().getTime()
+    createdAt : date.valueOf()
   }
 };
 var generateLocationMessage = (from , latitude, longitude) => {
   return{
     from,
     url : `https://www.google.com/maps?q=${latitude},${longitude}`,
-    createdAt : new Date().getTime()
+    createdAt : date.valueOf()
   }
 };
 module.exports = {generateMessage,generateLocationMessage};
